923b56ce94bb45225925ebf3c59c67d9f547ed5c
justTrueCodeWriter <dmjustcoder8@gmail.com>
2025-09-09 22:14:45 +0300

    upd

--- lab1/polybian_square.py
+++ lab1/polybian_square.py
@@ -1,38 +1,37 @@
+import argparse
+
 # polybian_square --key key.txt --encrypt plaintext.txt --output encrypted.txt
 # polybian_square --key key.txt --decrypt encrypted.txt --output decrypted.txt
 
-ALPHABET = "ЩС0УФ8ЪЖГ.4ПЯ Р9ЛЗВ,Ь:3ЧШТЁЭ1ОЙД5ЫИ6-НКАБМ2ЦЕЮ7Х"
+#ALPHABET = "ЩС0УФ8ЪЖГ.4ПЯ Р9ЛЗВ,Ь:3ЧШТЁЭ1ОЙД5ЫИ6-НКАБМ2ЦЕЮ7Х"
 
-assert len(ALPHABET) == 48, f"Ожидалось 48 символов, получили {len(ALPHABET)}"
+#assert len(ALPHABET) == 48, f"Ожидалось 48 символов, получили {len(ALPHABET)}"
 
-rows = 6
-cols = 8
-
-def encrypt(plaintext: str, key: str):
+def encrypt(plaintext: str, key: str, cols: int):
     encrypted_text = ""
 
     for character in plaintext:
         pos = key.find(character)
-        encrypted_text += key[(pos+rows)%len(key)]
+        encrypted_text += key[(pos+cols)%len(key)]
 
     print(*encrypted_text, sep="")
     return encrypted_text
 
 
-def decrypt(encrypted_text: str, key: str):
+def decrypt(encrypted_text: str, key: str, cols: int):
     decrypted_text = ""
    
     for character in encrypted_text:
         pos = key.find(character)
-        decrypted_text += key[(pos-rows)%len(key)]
+        decrypted_text += key[(pos-cols)%len(key)]
 
     print(*decrypted_text, sep="")
     return decrypted_text
 
-def print_alphabet(key: str):
+def print_alphabet(key: str, cols: int):
     i = 0
     for character in key:
-        if i == 6:
+        if i == cols:
             print("")
             i = 0
         print(character, end=" ")
@@ -47,9 +46,45 @@ def write_file(output: str, filename: str):
     with open(filename, "w") as file:
         return file.write(output)
 
-def cypher_cli():
+def cipher_cli():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--key", type=str)
+    parser.add_argument("--encrypt", type=str)
+    parser.add_argument("--decrypt", type=str)
+    parser.add_argument("--output", type=str)
+    parser.add_argument("--cols", type=int)
+    parser.add_argument("pkey", required=False)
+
+    args = parser.parse_args()
+
+    if args is None:
+        parser.print_help()
+        return
+
+    if args.pkey:
+        key = read_file(args.key)
+        print_alphabet(key, args.cols)
+    
+    if args.encrypt:
+        plaintext = read_file(args.encrypt)
+        key = read_file(args.key)
+        cols = args.cols
+        output_filename = args.output
+
+        cipher_text = encrypt(plaintext, key, cols)
+        write_file(cipher_text, output_filename)
+
+    elif args.decrypt:
+        encrypted_text = read_file(args.decrypt)
+        key = read_file(args.key)
+        cols = args.cols
+        output_filename = args.output
+
+        decrypted_text = decrypt(encrypted_text, key, cols)
+        write_file(decrypted_text, output_filename)
+      
     return
 
-
-#write_file(encrypt(read_file("plaintext.txt"), read_file("key.txt")), "encrypt.txt")
-write_file(decrypt(read_file("encrypt.txt"), read_file("key.txt")), "decrypt.txt")
+if __name__ == "__main__":
+    cipher_cli()
