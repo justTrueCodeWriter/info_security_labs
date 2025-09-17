e1186511750b1b81439af70c9269800573d70d93
justTrueCodeWriter <dmjustcoder8@gmail.com>
2025-09-17 19:43:28 +0300

    upd

--- lab2/main.py
+++ lab2/main.py
@@ -7,6 +7,11 @@ M = len(SYMBOLYK_TABLE)
 
 INDEX = {ch: i for i, ch in enumerate(SYMBOLYK_TABLE)}
 
+def check_text_chars(text: str):
+    bad = [ch for ch in text if ch not in INDEX]
+    if bad:
+        raise ValueError(f"В тексте есть символы, отсутствующие в алфавите: {set(bad)}")
+
 def generate_key(plaintext: str) -> str:
     plaintext_len = len(plaintext)  
     
@@ -19,28 +24,37 @@ def generate_key(plaintext: str) -> str:
 
     return key
 
-def encrypt(plaintext: str, key: str):
+def encrypt(plaintext: str, key: str, start: int):
     
     out_chars = []
 
+    key_len = len(key)
+
+    if start < 0 or start >= key_len:
+        raise ValueError("Начальная позиция вне диапазона ключа")
+
     for i, ch in enumerate(plaintext):
         mi = INDEX[ch]
-        ki = INDEX[key[i]]
+        ki = INDEX[key[(start+i)%key_len]]
         ci = (mi+ki)%M
         out_chars.append(SYMBOLYK_TABLE[ci])
-
 
     cipher_text = "".join(out_chars)
 
     return cipher_text
 
-def decrypt(encrypted_text: str, key: str):
+def decrypt(encrypted_text: str, key: str, start: int):
 
     out_chars = []
 
+    key_len = len(key)
+
+    if start < 0 or start >= key_len:
+        raise ValueError("Начальная позиция вне диапазона ключа")
+
     for i, ch in enumerate(encrypted_text):
         ci = INDEX[ch]
-        ki = INDEX[key[i]]
+        ki = INDEX[key[(start+i)%key_len]]
         mi = (ci-ki)%M
         out_chars.append(SYMBOLYK_TABLE[mi])
 
@@ -71,60 +85,54 @@ def cipher_cli():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--key", type=str, help="key filename")
-    parser.add_argument("--input", type=str, help="input filename")
     parser.add_argument("--output", type=str, help="output filename")
-    parser.add_argument("--pkey", default="", help="print key")
-    parser.add_argument("--encrypt", default="", help="encrypt data")
-    parser.add_argument("--decrypt", default="", help="decrypt data")
-    parser.add_argument("--genkey", default="", help="generate key")
+    parser.add_argument("--pkey", nargs="?", help="print key")
+    parser.add_argument("--encrypt", type=str, help="encrypt filename")
+    parser.add_argument("--decrypt", type=str, help="decrypt filename")
+    parser.add_argument("--genkey", type=str, help="generate key")
+    parser.add_argument("--shift", type=int, default=0, help="generate key")
 
     args = parser.parse_args()
-
-    print(args)
 
     if args is None:
         parser.print_help()
         return
 
     if args.genkey:
-        print(args.input)
-        # plaintext = read_file(args.input)
-        # output_filename = args.output
-        # print(output_filename)
-        #
-        # key = generate_key(plaintext)
-        # print(key)
-        #
-        # write_file(plaintext, output_filename)
+        plaintext = read_file(args.genkey)
+        output_filename = args.output
+        key = generate_key(plaintext)
+
+        write_file(key, output_filename)
          
     if args.pkey:
         key = read_file(args.key)
         print_alphabet(key, args.cols)
     
     if args.encrypt:
-        plaintext = read_file(args.input)
+        plaintext = read_file(args.encrypt)
         key = read_file(args.key)
         output_filename = args.output
 
-        cipher_text = encrypt(plaintext, key)
+        cipher_text = encrypt(plaintext, key, args.shift )
         write_file(cipher_text, output_filename)
 
     elif args.decrypt:
-        encrypted_text = read_file(args.input)
+        encrypted_text = read_file(args.decrypt)
         key = read_file(args.key)
         output_filename = args.output
 
-        decrypted_text = decrypt(encrypted_text, key)
+        decrypted_text = decrypt(encrypted_text, key, args.shift)
         write_file(decrypted_text, output_filename)
       
     return
 
 if __name__ == "__main__":
-    #cipher_cli()
-    plaintext = 'i wanna know if you ever seen rain'
-    key = generate_key(plaintext)
-    #key = "\"+$!(#%'* &)"
-    
-    cipher_text = encrypt(plaintext, key)
-    print(cipher_text)
-    print(decrypt(cipher_text, key))
+    cipher_cli()
+    # plaintext = 'i wanna know if you ever seen rain'
+    # key = generate_key(plaintext)
+    # #key = "\"+$!(#%'* &)"
+    #
+    # cipher_text = encrypt(plaintext, key, 2)
+    # print(cipher_text)
+    # print(decrypt(cipher_text, key, 2))
